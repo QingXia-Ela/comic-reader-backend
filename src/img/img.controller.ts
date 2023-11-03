b7c7be32920cf1dfd7c8846f19555c5d340abde9
QingXia-Ela <Elahere@163.com>
2023-11-03 21:39:50 +0800

    feat(img): 增加获取图片的基本接口

--- src/img/img.controller.ts
+++ src/img/img.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { readFile } from 'fs-extra';
+import { DbService } from 'src/db/db.service';
 
 @Controller('img')
 export class ImgController {
+  constructor(private dbService: DbService) {}
   @Get(':id/:img')
   getImg(@Param() { id, img }) {
-    return '';
+    return readFile(`book/${this.dbService.getDbMapVal(id).title}/${img}`);
   }
 }
--- src/img/img.module.ts
+++ src/img/img.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ImgController } from './img.controller';
+import { DbService } from 'src/db/db.service';
 
 @Module({
   controllers: [ImgController],
+  providers: [DbService],
 })
 export class ImgModule {}
