eae8ff673ee63552f0f069af6dfdece43b419eb7
QingXia-Ela <Elahere@163.com>
2023-11-03 21:29:35 +0800

    fix(list): 修复获取最新漫画接口没被简化的问题

--- src/list/list.service.ts
+++ src/list/list.service.ts
@@ -1,25 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
+import getSimplifyComic from './utils/getSimplifyComic';
 
 @Injectable()
 export class ListService {
   constructor(private dbService: DbService) {}
   getNewest(count: number) {
-    return this.dbService.getNewest();
+    return this.dbService.getNewest().map(getSimplifyComic);
   }
 
   getList(count: number, offset: number) {
     const list = this.dbService.getList(count, offset);
     return {
       hasMore: list.length + offset < this.dbService.getComicCount(),
-      data: list.map((comic) => ({
-        id: comic.id,
-        title: comic.title,
-        cover: comic.cover,
-        date: comic.date,
-        tags: comic.tags,
-        description: comic.description,
-      })),
+      data: list.map(getSimplifyComic),
     };
   }
 }
---
+++ src/list/utils/getSimplifyComic.ts
@@ -0,0 +1,12 @@
+import { CreateComicDto } from '../dto/create-comic.dto';
+
+export default function (comic: CreateComicDto) {
+  return {
+    id: comic.id,
+    title: comic.title,
+    cover: comic.cover,
+    date: comic.date,
+    tags: comic.tags,
+    description: comic.description,
+  };
+}
