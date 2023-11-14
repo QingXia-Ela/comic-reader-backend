7f62ffa908b7d4ef84b6c12b20fc79d84eb1ed0e
QingXia-Ela <Elahere@163.com>
2023-11-14 22:05:50 +0800

    feat: 优化图片读取处理

--- src/img/img.controller.ts
+++ src/img/img.controller.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs-extra';
 import { Controller, Get, HttpException, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import settingsCustom from 'settings.custom';
@@ -7,34 +8,29 @@ import { DbService } from 'src/db/db.service';
 export class ImgController {
   constructor(private dbService: DbService) {}
   @Get(':id/:img')
-  getImg(@Param() { id, img }, @Res() res: Response) {
-    // return readFile(`book/${this.dbService.getDbMapVal(id).title}/${img}`);
-    return new Promise<void>((resolve, rej) => {
+  async getImg(@Param() { id, img }, @Res() res: Response) {
+    const path = settingsCustom.crypto.active
+      ? `${this.dbService.getDbMapVal(id).title}/encrypted/${img}.buf`
+      : `${this.dbService.getDbMapVal(id).title}/${img}`;
+    try {
+      await fs.lstat(`book/${path}`);
+    } catch (e) {
+      throw new HttpException('img not found', 404);
+    }
+    await new Promise<void>((resolve, rej) => {
       res.setHeader('cache-control', 'max-age=30000');
-      if (settingsCustom.crypto.active) {
+      if (settingsCustom.crypto.active)
         res.setHeader('content-type', 'application/octet-stream');
-        res.sendFile(
-          `${this.dbService.getDbMapVal(id).title}/encrypted/${img}.buf`,
-          {
-            root: 'book',
-          },
-          (err) => {
-            if (err) rej(new HttpException('img not found', 404));
-            resolve();
-          },
-        );
-      } else {
-        res.sendFile(
-          `${this.dbService.getDbMapVal(id).title}/${img}`,
-          {
-            root: 'book',
-          },
-          (err) => {
-            if (err) rej(new HttpException('img not found', 404));
-            resolve();
-          },
-        );
-      }
+      res.sendFile(
+        path,
+        {
+          root: 'book',
+        },
+        (err) => {
+          if (err) rej('');
+          else resolve();
+        },
+      );
     });
   }
 }
