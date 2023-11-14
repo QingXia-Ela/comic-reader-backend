7f09bde55ab66d90f4985987cc569ef10d636c12
QingXia-Ela <Elahere@163.com>
2023-11-14 18:24:53 +0800

    feat: 优化错误抛出

--- src/img/img.controller.ts
+++ src/img/img.controller.ts
@@ -9,28 +9,32 @@ export class ImgController {
   @Get(':id/:img')
   getImg(@Param() { id, img }, @Res() res: Response) {
     // return readFile(`book/${this.dbService.getDbMapVal(id).title}/${img}`);
-    res.setHeader('cache-control', 'max-age=30000');
-    if (settingsCustom.crypto.active) {
-      res.setHeader('content-type', 'application/octet-stream');
-      return res.sendFile(
-        `${this.dbService.getDbMapVal(id).title}/encrypted/${img}.buf`,
-        {
-          root: 'book',
-        },
-        (err) => {
-          if (err) res.sendStatus(404);
-        },
-      );
-    } else {
-      return res.sendFile(
-        `${this.dbService.getDbMapVal(id).title}/${img}`,
-        {
-          root: 'book',
-        },
-        (err) => {
-          if (err) res.sendStatus(404);
-        },
-      );
-    }
+    return new Promise<void>((resolve, rej) => {
+      res.setHeader('cache-control', 'max-age=30000');
+      if (settingsCustom.crypto.active) {
+        res.setHeader('content-type', 'application/octet-stream');
+        res.sendFile(
+          `${this.dbService.getDbMapVal(id).title}/encrypted/${img}.buf`,
+          {
+            root: 'book',
+          },
+          (err) => {
+            if (err) rej(new HttpException('img not found', 404));
+            resolve();
+          },
+        );
+      } else {
+        res.sendFile(
+          `${this.dbService.getDbMapVal(id).title}/${img}`,
+          {
+            root: 'book',
+          },
+          (err) => {
+            if (err) rej(new HttpException('img not found', 404));
+            resolve();
+          },
+        );
+      }
+    });
   }
 }
